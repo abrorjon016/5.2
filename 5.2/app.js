f6f393999d40cfde08bdd51052550a2472af40fb
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-11-25 11:17:23 +0500

    chala bolib qoldi yana push qilib qoyamab

---
+++ 5.2/app.js
@@ -0,0 +1,41 @@
+// 1-masala
+// function bugatti(arr) {
+//   return arr.split("").reverse().join("");
+// }
+
+// 3-masala
+// function bugatti(arr) {
+//   let bigs = "ASDFGHJKLZXCVBNMQWERTYUIOP";
+//   let counter = 0;
+//   for (let char of arr) {
+//     if (bigs.includes(char)) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
+
+// 4-masala
+// function bugatti(arr) {
+//   return arr
+//     .split(" ")
+//     .map((shiron) => shiron.charAt(0).toUpperCase() + shiron.slice(1))
+//     .join(" ");
+// }
+
+// 5-masala
+// function bugatti(arr, shiron, tourbillion) {
+//   return str.replace(shiron, tourbillion);
+// }
+
+// 8-masala
+// function bugatti(arr) {
+//   return str
+//     .split(" ")
+//     .map((shiron) => shiron.split("").reverse().join(""))
+//     .join(" ");
+// }
+
+// ustoz functionga oidlari qiyin boldi bularni organbib bugun qilib albatta bajaraib qoyaman
+// kech qolishim uchun uzr sorayman 
+
